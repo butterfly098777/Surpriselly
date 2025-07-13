a0d9ffa1743dac01735dc59dfa95b6f94de2d35b
PallaviPatel <153287259+butterfly098777@users.noreply.github.com>
2025-07-13 19:40:22 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -26,7 +26,7 @@ export default function Cart() {
   const handleCheckout = async () => {
     window.location.href="http://razorpay.me/@socialoffer";
     try {
-      const res = await fetch("http://localhost:5000/api/payment/create-order", {
+      const res = await fetch("https://surpriselly.onrender.com/api/payment/create-order", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
