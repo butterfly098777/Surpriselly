197b50f572cd1a8c6442afa3c52ac008697ef702
PallaviPatel <153287259+butterfly098777@users.noreply.github.com>
2025-07-13 23:09:21 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -12,7 +12,7 @@ export default function Signup() {
  const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const res = await fetch("http://localhost:5000/api/auth/signup", {
+    const res = await fetch("https://surpriselly.onrender.com/api/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user),
