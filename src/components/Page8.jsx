d8da032c3926844944939ef707daba9e42a48fcf
PallaviPatel <153287259+butterfly098777@users.noreply.github.com>
2025-07-13 23:40:39 +0530

    Update Page8.jsx

--- src/components/Page8.jsx
+++ src/components/Page8.jsx
@@ -99,7 +99,7 @@ export default function Page8() {
     src={
       item.image.startsWith("http")
         ? item.image
-        : `http://localhost:5000${item.image}`
+        : `https://surpriselly.onrender.com${item.image}`
     }
     alt={item.title || item.name}
     className="w-full h-44 object-cover rounded-lg"
